9c9ee8023b0b6f5aa9dc33ef1cc07e76a9ab626f
yanmo <67538999+yanmoyy@users.noreply.github.com>
2025-05-22 08:35:22 +0900

    add filepath to server (index.html)

--- main.go
+++ main.go
@@ -8,13 +8,16 @@ import (
 func main() {
 
 	const port = "8080"
+	const filepathRoot = "."
 
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 
 	srv := http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
-	log.Printf("Serving on port: %s\n", port)
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 }
