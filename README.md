341ad59ed05747f1e16b18b67e499af9bf6f8330
yanmo <67538999+yanmoyy@users.noreply.github.com>
2025-05-22 11:23:45 +0900

    specify http methods

---
+++ README.md
@@ -0,0 +1,15 @@
+# GO-HTTP-SERVER
+
+- Simple HTTP Server in Go for study ([boot.dev](https://boot.dev))
+
+## Goal of This Project
+
+- Understand what web server are and how they power real-world web application
+- Build a production-style HTTP server in Go, without the use of `framework`
+- Use JSON, headers, and status codes to communicate with clients via a
+  `RESTful API`
+- Use type safe `SQL` to store and retrieve data from a Postgres database
+- Implement a secure `authentication/authorization` system with well-tested
+  cryptography libraries
+- Build and understand webhooks and API keys
+- Document the `REST API` with markdown
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"sync/atomic"
@@ -14,16 +13,14 @@ type apiConfig struct {
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
-
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
 	}
-
 	mux := http.NewServeMux()
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
-	mux.HandleFunc("/healthz", handlerReadiness)
-	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("/reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /healthz", handlerReadiness)
+	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /reset", apiCfg.handlerReset)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -32,17 +29,4 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
-}
-
-func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
 }
